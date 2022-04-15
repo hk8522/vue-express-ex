bfb59d1d790b27e93534eae748e7eb8c3c37f8cf
hk85222 <94052790+hk8522@users.noreply.github.com>
2022-04-15 09:39:58 +0900

    Server create

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const { networkInterfaces } = require('os');
+
+require('dotenv').config();
+
+const nets = networkInterfaces();
+
+var privateKey  = fs.readFileSync('ssl/private.key', 'utf8');
+var certificate = fs.readFileSync('ssl/certificate.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Behold The MEVN Stack!'
+    });
+});
+
+const port = process.env.PORT || 4000;
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.on('listening', function () {
+    console.log('Server running at http://' + get_ip() + ':' + this.address().port);
+});
+httpsServer.on('listening', function () {
+    console.log('Server running at https://' + get_ip() + ':' + this.address().port);
+});
+
+httpServer.listen(process.env.PORT || 8080);
+httpsServer.listen(process.env.PORT_HTTPS || 8443);
+
+function get_ip() {
+    // const results = Object.create(null);
+    // for (const name of Object.keys(nets)) {
+    //     for (const net of nets[name]) {
+    //         // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    //         if (net.family === 'IPv4' && !net.internal) {
+    //             if (!results[name]) {
+    //                 results[name] = [];
+    //             }
+    //             results[name].push(net.address);
+    //         }
+    //     }
+    // }
+    // console.log(results);
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return "127.0.0.1";
+}
